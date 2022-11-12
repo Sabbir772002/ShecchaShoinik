3d8508331761e25f493f7b0ad866de928299cf26
Sabbir <Sabbir772002@gmail.com>
2022-11-13 00:15:55 +0600

    new commit at 12:25

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,12 +7,18 @@ module com.example.sheccashoinik {
     opens com.example.sheccashoinik to javafx.fxml;
     exports com.example.sheccashoinik;
 
+    opens BloodBank to javafx.fxml;
+    exports BloodBank;
+
     opens Sign_in to javafx.fxml;
     exports Sign_in;
+
     opens Sign_UP to javafx.fxml;
     exports Sign_UP;
+
     opens DB to javafx.fxml;
     exports DB;
+
     opens AdminDB to javafx.fxml;
     exports AdminDB;
 }
