1499ebcf8843988ad2e8ee1c03242ca66dec3ccd
Sabbir <Sabbir772002@gmail.com>
2022-11-12 19:40:31 +0600

    new change

---
+++ src/main/java/com/example/sheccashoinik/Application.java
@@ -0,0 +1,27 @@
+package com.example.sheccashoinik;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HomeController.class.getResource("Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        stage.setTitle("ShecchaShoinik");
+        stage.setScene(scene);
+        File file = new File("src/main/Font/icon1.png");
+        Image image = new Image(file.toURI().toString());
+        stage.getIcons().add(image);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/sheccashoinik/HomeController.java
@@ -0,0 +1,51 @@
+package com.example.sheccashoinik;
+
+import Sign_in.Sign_inController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HomeController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    void createvt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Signin(ActionEvent event) {
+        try {
+            root = FXMLLoader.load(Sign_inController.class.getResource("Sign_in.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("SIGN IN");
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void SignUp (ActionEvent event) {
+        try {
+            root = FXMLLoader.load(Sign_UP.SignupController.class.getResource("Sign_UP.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("SIGN UP");
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
